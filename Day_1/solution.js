c9a24fadcf51e6b98e6b645214271c4c8fada9fb
Phil Sung <phil.sung@aligent.com.au>
2022-12-21 16:41:44 +1030

    AOC-1: Adds initial solution. Passed test

--- Day_1/solution.js
+++ Day_1/solution.js
@@ -2,7 +2,22 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
   });
 
-console.log(rl.input);
+  let highestTotal = 0;
+  let currentTotal = 0;
+
+  rl.on('line', (line) => {
+    const calories = parseInt(line);
+    if(!calories) {
+        if (highestTotal < currentTotal) {
+            highestTotal = currentTotal;
+        }
+        currentTotal = 0;
+    }
+    else currentTotal += calories;
+  });
+
+  rl.on('close', () => {
+    console.log(highestTotal);
+  })
